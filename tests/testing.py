3aece3032f6431bd338f9fad616d4221faa369d5
Merve <3mervekilic@gmail.com>
2020-12-10 08:48:21 -0800

    edit

--- tests/testing.py
+++ tests/testing.py
@@ -6,7 +6,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(multiply_by_2(5), 10)
 
     def test_something2(self):
-        self.assertEqual(multiply_by_2(4), 8)
+        self.assertEqual(multiply_by_2(4), 7)
 
 if __name__ == '__main__':
     unittest.main()
